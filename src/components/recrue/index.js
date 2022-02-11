4b7433dede17d7cda3988b75b6cb890e443612f0
rourou <rouillard.romain@gmail.com>
2022-02-11 21:03:56 +0100

    Margin in recrute

--- src/components/recrue/index.js
+++ src/components/recrue/index.js
@@ -9,7 +9,7 @@ const Recrue = (props) => {
 
 
     return (
-        <div className="lg:flex m-5 align-content-center md:block align-content-center align-items-stretch text-center justify-content-center">
+        <div className="lg:flex sm:m-2 lg:m-5 align-content-center md:block align-content-center align-items-stretch text-center justify-content-center">
             <div>
                 <img className="sm:m-1 lg:m-2 border-round"
                     alt="IMG recrutement"
